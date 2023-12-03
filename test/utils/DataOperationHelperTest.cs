4312316c9f570e50c7fa33c3591b8edfb2ae05f5
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-12-02 22:36:12 -0500

    first commit

---
+++ test/utils/DataOperationHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketReverseShellDotNet.utils;
+
+namespace WebSocketReverseShellDotNet.test.utils
+{
+    [TestFixture]
+    internal class DataOperationHelperTest
+    {
+
+        [Test]
+        public void StringToList_ValidInput_ReturnsList()
+        {
+            // Arrange
+            string inputString = "item1,item2,item3";
+            string delimiter = ",";
+
+            // Act
+            List<string> result = DataOperationHelper.StringToList(inputString, delimiter);
+            Console.WriteLine(result.ToString()); 
+            result.ForEach(x => Console.WriteLine(x));
+            // Assert
+            CollectionAssert.AreEquivalent(new[] { "item1", "item2", "item3" }, result);
+        }
+    }
+}
